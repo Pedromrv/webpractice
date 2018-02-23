70bc873faaf290e846315d8e24cd70903cc763a7
Pedromrv <pedromrv@gmail.com>
2018-02-23 16:28:22 +0100

    viernes23-2nd

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ var myImage = document.querySelector('img');
 myImage.onclick = function() {
 	var mySrc = myImage.getAttribute('src');
 	if(mySrc === 'images/firefox-icon.png') {
-      myImage.setAttribute ('src','images/firefox2.png');
+      myImage.setAttribute ('src','images/beard.png');
 	} else {
 	  myImage.setAttribute ('src','images/firefox-icon.png');
 	}
