ba016783f23f06a05cb03e5e9a24481fa312bd78
Pedro <32385095+Pedromrv@users.noreply.github.com>
2018-03-02 11:20:29 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -19,14 +19,14 @@ var myHeading = document.querySelector('h1');
 function setUserName() {
   var myName = prompt('Please enter your name.');
   localStorage.setItem('name', myName);
-  myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+  myHeading.innerHTML = 'Beard is cool, ' + myName;
 }
 
 if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   var storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+  myHeading.innerHTML = 'Beard is cool, ' + storedName;
 }
 
 myButton.onclick = function() {
